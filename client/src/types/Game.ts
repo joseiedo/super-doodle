67d12ac4540204c7b83a93ab122381c72636d764
José Iêdo <iedoduarte1@gmail.com>
2023-06-07 17:03:36 -0300

    add interfaces

---
+++ client/src/types/Game.ts
@@ -0,0 +1,6 @@
+import { Player } from './Player';
+
+export interface Game {
+  id: number;
+  players: Player[];
+}
---
+++ client/src/types/Player.ts
@@ -0,0 +1,5 @@
+export interface Player {
+  id: number;
+  positionY: number;
+  positionX: number;
+}
